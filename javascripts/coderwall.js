1bcd271488de20bd97f6b7e26c7d0da3066a8def
tzangms <tzangms@gmail.com>
2012-02-15 12:39:29 +0800

    Site updated at 2012-02-15 04:39:29 UTC

--- javascripts/coderwall.js
+++ javascripts/coderwall.js
@@ -10,31 +10,35 @@ var coderwallJSONurl ="http://www.coderwall.com/tzangms.json?callback=?";
 var height = 85;
 var width = 85;	
 
-$(function(){
-	$.getJSON(coderwallJSONurl, function(data) {
-		
-		$.each(data.data.badges, function(i, item) {
-		
-			$("<img/>").attr("src", item.badge)
-			.attr("float", "left")
-			.attr("title", item.name + ": " + item.description)
-			.attr("alt", item.name)
-			.attr("height", height)
-			.attr("width", width)
-			.appendTo("#coderwall")
-			.hover(
-				function(){  
-					$(this).css("opacity","0.6");
-				},
-				function(){
-					$(this).css("opacity","1.0");
-				}
-			)
-			.click(
-				function(){
-					window.location = "http://www.coderwall.com/tzangms/";
-				}
-			);		
-		});
+$.domReady(function(){
+	$.ajax({
+        url: coderwallJSONurl, 
+        data: 'json',
+        success: function(data) {
+            console.log(data);
+            
+            $.each(data.data.badges, function(i, item) {
+                $("<img>").attr("src", item.badge)
+                .attr("float", "left")
+                .attr("title", item.name + ": " + item.description)
+                .attr("alt", item.name)
+                .attr("height", height)
+                .attr("width", width)
+                .appendTo("#coderwall")
+                .hover(
+                    function(){  
+                        $(this).css("opacity","0.6");
+                    },
+                    function(){
+                        $(this).css("opacity","1.0");
+                    }
+                )
+                .click(
+                    function(){
+                        window.location = "http://www.coderwall.com/tzangms/";
+                    }
+                );
+            });
+        }
 	});
 });
